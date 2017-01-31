f25f05a400acace355ddd2ac4fa087becac78554
ElliotRourke <elliot.rourke@gmail.com>
2017-01-31 18:58:56 +0000

    Begun creating the command loop for the shell!

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
-void int main(int argc, char const *argv[]) {
-  
+//Define global variables
+
+
+//Define functions
+void command_loop();
+
+//This is a simple OS shell designed by our team!
+int main(int argc, char const *argv[])
+{
+  //Firstly initialize the shell - load any configs etc
+
+  //Secondly enter "command loop"
+  command_loop();
+  //Perform clean-up for exit/shut-down
+
+
   return 0;
+}
+
+//Handles commands input by user
+void command_loop()
+{
+  int status;
+
+  do{
+    //Read
+    //Parse
+    //Execute
+
+  }while (status);
+
 }
