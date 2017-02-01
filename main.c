5a4da51def1da5b3bab2c664ef5b60dac208001a
ElliotRourke <elliot.rourke@gmail.com>
2017-02-01 18:39:49 +0000

    Completed parse_input main functionality - still need to add error checking for malloc(?)

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /* Define constants */
 #define STRINGSIZE 512
 
 /* Define functions */
 void command_loop();
-char * read_input();
-char ** parse_input(char *input);
+char *read_input();
+char **parse_input(char *input);
 
 //This is a simple OS shell designed by our team!
 int main(int argc, char const *argv[])
@@ -42,7 +44,7 @@ void command_loop()
 }
 
 //Reads in user input
-char * read_input()
+char *read_input()
 {
   size_t buffer_size = STRINGSIZE;
   char * line = NULL;
@@ -53,12 +55,28 @@ char * read_input()
 
 //Parses user input
 // Personal note ( This function is a pointer that points to pointers)
-char ** parse_input(char *input)
+char **parse_input(char *input)
 {
-  char * token;
 
-  //strtok used here
+  int bufsize = STRINGSIZE;
+  int index = 0;
+  char delimiters[] = " \t | > < & ;";
+  char *token;
+  char **tokens = malloc(bufsize * sizeof(char*));
 
+  token = strtok(input,delimiters);
+
+  while( token != NULL){
+      tokens[index] = token;
+      index++;
+      /* *******   TEST PRINT PLEASE REMOVE WHEN COMPLETE     ******** */
+      printf("%s\n",token);
+      token = strtok(NULL, delimiters);
+  }
+
+  tokens[index] = NULL;
+
+  return tokens;
 }
 
 int execute_input(){
